bdd47025b066d992cf53d5370c6ebc68aec0c851
HowNeft <92006941+HowNeft@users.noreply.github.com>
2022-02-09 10:59:17 +0700

    remove

--- components/change-image/app.js
+++
@@ -1,7 +0,0 @@
-const images = document.querySelectorAll(".list img");
-images.forEach((el) => el.addEventListener("click", handleChangeImage));
-function handleChangeImage(e) {
-  const source = e.target.getAttribute("src");
-  const featureImage = document.querySelector(".feature-image");
-  featureImage.setAttribute("src", source);
-}
